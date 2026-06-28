a68c69e8e5059545ac9e625316a4f62bf4e18992
divyanshyadav87 <dsy16325@gmail.com>
2026-06-28 12:01:08 +0530

    facebook Link Corrected

--- app/page.tsx
+++ app/page.tsx
@@ -1022,7 +1022,7 @@ export default function ComingSoonPage() {
             </motion.a>
 
             <motion.a
-              href="https://www.facebook.com/share/1JD3wqwnqZ/"
+              href="https://www.facebook.com/share/1ExJihbVYe/"
               target="_blank"
               rel="noopener noreferrer"
               whileHover={{ y: -2, scale: 1.06 }}
